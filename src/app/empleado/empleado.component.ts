467fb02d6c448bdcc39ca94e563ac53531a893e3
Marito048 <estudio1802@gmail.com>
2022-02-10 20:58:48 -0300

    commit

--- src/app/empleado/empleado.component.ts
+++ src/app/empleado/empleado.component.ts
@@ -12,9 +12,19 @@ export class EmpleadoComponent implements OnInit {
   apellido= "Diaz";
   edad= 18;
   habilitacionCuadro=false;
-  usuRegistrado=true;
+  usuRegistrado=false;
+  textoDeRegistro="No hay nadie registrado";
+
   
   //empresa= "Pildoras Informaticas"
+  setUsuarioRegistrado(){
+    //alert("El Usuario se acaba de registrar");
+    this.textoDeRegistro="El usuario se acaba de registrar";
+  }
+  
+
+
+  
  
   constructor() { }
 
